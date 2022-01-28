41b79a3744ffa8c9561ce86abad1fe5cd3fdc5a5
Salam Salehpour <SalameSalehpour@gmail.com>
2022-01-28 00:50:13 -0800

    Add routing

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const fs = require('fs');
 const port = 5000;
 
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('index.html', function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.write('Error : File not found');
-        } else {
-            res.write(data);
-        }
+    if (req.url === '/') {
+        res.write('Hello world');
         res.end();
-    })
+    }
+
+    if (req.url === '/index.html') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('index.html', function (err, data) {
+            if (err) {
+                res.writeHead(404);
+                res.write('Error : File not found');
+            } else {
+                res.write(data);
+            }
+            res.end();
+        })
+    }
 });
 
 server.listen(port, function (error) {
